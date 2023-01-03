dc25b27a3d9c0554fe07893ce2e9ea1cea0a1f05
Luiz Porto <piilamster@gmail.com>
2023-01-03 18:49:59 -0300

    section our products

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
 import { Header } from "./components/header/Header";
 import Hero from "./components/Hero/Hero";
+import Products from "./components/Products/Products";
 import Slider from "./components/Slider/Slider";
 import  Virtual  from "./components/Virtual/Virtual";
+
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
       <Hero/>
       <Slider/>
       <Virtual/>
+      <Products/>
     </div>
   );
 }
---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+import css from './Products.module.css'
+import Plane from '../../assets/plane.png'
+import { ProductsData } from '../../data/products'
+import { useAutoAnimate } from "@formkit/auto-animate/react";
+
+const Products = () => {
+    const [parent] = useAutoAnimate()
+
+    const [MenuProducts, setMenuProducts ] = useState(ProductsData)
+
+    const filter = (type) => {
+        setMenuProducts(ProductsData.filter((product)=>product.type === type))
+    }
+
+  return (
+    <div className={css.container}>
+        <img src={Plane} alt="" />
+        <h1>Our Featured Products</h1>
+
+        <div className={css.products}>
+            <ul className={css.menu}>
+               <li onClick={()=>setMenuProducts(ProductsData)}>All</li>
+               <li onClick={()=>filter("skin care")}>Skin Care</li>
+               <li onClick={()=>filter("conditioner")}>Conditioners</li>
+               <li onClick={()=>filter("foundation")}>Foundations</li> 
+            </ul>
+
+            <div className={css.list} ref={parent}>
+                {
+                    MenuProducts.map((product, i) => (
+                        <div className={css.product}>
+                            <div className="left-s">
+                               <div className='name'>
+                                   <span>{product.name}</span>
+                                   <span>{product.detail}</span> 
+                               </div> 
+                               <span>{product.price}$</span>
+                               <div>Show Now</div>
+                            </div>
+
+                            <img src={product.img} alt='' className='img-p' />
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Products
