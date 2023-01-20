ee33302a99dbdd715a9d6f5d5b385c8a70586290
Luiz Porto <piilamster@gmail.com>
2023-01-20 16:10:56 -0300

    responsive menu

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import css from "./Header.module.css";
 import Logo from "../../assets/logo.png";
 import { CgShoppingBag } from "react-icons/cg";
+import { GoThreeBars } from "react-icons/go";
 
 export const Header = () => {
+
+  const [ShowMenu, setShowMenu] = useState(true)
+
+  const toggleMenu = ( ) => {
+      setShowMenu((ShowMenu)=>!ShowMenu)
+  }
   return (
     <div className={css.container}>
         <div className={css.logo}>
@@ -11,16 +18,22 @@ export const Header = () => {
            <span>amazon</span>
         </div>
 
+
         <div className={css.right}>
-           <div className={css.menu}>
-                <ul className={css.menu}>
+
+           <div className={css.bars} onClick={toggleMenu}>
+              <GoThreeBars/>
+           </div> 
+
+           
+                <ul className={css.menu} style={{display: ShowMenu? 'inherit': 'none'}}>
                   <li>Collections</li>
                   <li>Brands</li>  
                   <li>New</li>  
                   <li>Sales</li>  
                   <li>ENG</li>  
                 </ul>
-           </div> 
+            
            <input type="text" className={css.search} placeholder="Search" />
 
            <CgShoppingBag className={css.cart}/>
